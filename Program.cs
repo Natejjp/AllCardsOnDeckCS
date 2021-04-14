2008b1967ef09267e38d4c2a05079efb3b8ba744
Nathan Phame <diyf2021@gmail.com>
2021-04-14 13:13:59 -0400

    Generates a random card through two different variables suites and numbers

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+
+            var random = new Random();
+
+            var cardSuites = new List<string>() { "Hearts", "Diamonds", "Spades", "Clubs" };
+            var cardNumbers = new List<string>() { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+            int index = random.Next(cardSuites.Count);
+
+            Console.WriteLine(cardNumbers[index] + " of " + cardSuites[index]);
+
+
+
+        }
     }
-  }
 }
