0c66f0f6d45e646d0aec0faabaa2bd563674cdf8
Nathan Phame <diyf2021@gmail.com>
2021-04-14 16:13:29 -0400

    Finished Explorer Mode

--- Program.cs
+++ Program.cs
@@ -8,17 +8,38 @@ namespace AllCardsOnDeckCS
         static void Main(string[] args)
         {
 
-
-            var random = new Random();
-
             var cardSuites = new List<string>() { "Hearts", "Diamonds", "Spades", "Clubs" };
             var cardNumbers = new List<string>() { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
-
-            int index = random.Next(cardSuites.Count);
-
-            Console.WriteLine(cardNumbers[index] + " of " + cardSuites[index]);
+            var deck = new List<string>();
 
 
+            for (var indexSuites = 0; indexSuites < cardSuites.Count; indexSuites++)
+            {
+                for (var indexNumbers = 0; indexNumbers < cardNumbers.Count; indexNumbers++)
+                {
+                    var card = $"{cardNumbers[indexNumbers]} of {cardSuites[indexSuites]}";
+                    deck.Add(card);
+                }
+            }
+
+
+            var numberOfCards = (deck.Count);
+
+
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+            {
+                var leftIndex = new Random().Next(0, rightIndex);
+                var rightCard = deck[rightIndex];
+                deck[rightIndex] = deck[leftIndex];
+                deck[leftIndex] = rightCard;
+
+
+                foreach (var ShuffledDeck in deck)
+                {
+                    Console.WriteLine($"{ShuffledDeck}");
+                }
+
+            }
 
         }
     }
